87bd14309db80b96e778e6a7cfd386c65f4228c0
jaumecosta <jaumecostagomez@gmail.com>
2020-06-09 19:45:28 +0200

    Cambiar tamaño

---
+++ js/imegen.js
@@ -0,0 +1,23 @@
+
+// recoje la imagen
+const reducir = document.getElementById("logoLasr");
+
+
+//funcion que realizara el raton cuando pase por encima de la imagen
+function encojer(){
+   reducir.style.width = "20em";
+   reducir.style.height = "20em";
+
+
+}
+
+ //funcion que realizara el raton cuando sale de la imagen
+function tamaño_normal(){
+   reducir.style.width = "1280px";
+   reducir.style.height = "720px";
+
+ }
+ 
+ 
+ 
+
